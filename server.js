7fbdd606f4bac6b604dec2c706d35be73917c2b8
Abraham E. Tavarez <abetavarez@icloud.com>
2020-09-16 22:11:49 -0400

    Routes Created

--- server.js
+++ server.js
@@ -4,9 +4,36 @@ const dotenv = require('dotenv');
 //* Loads env variables
 dotenv.config({ path: './config/config.env' });
 
+//* Ints Express Server App
 const app = express();
 
+//* ROUTES
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show All Bootcamps' });
+});
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show Bootcamp ${req.params.id}` });
+});
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create New Bootcamp' });
+});
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update Bootcamp ${req.params.id}` });
+});
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete Bootcamp ${req.params.id}` });
+});
+
+//* Sets PORT
 const PORT = process.env.PORT || 5000;
+
+//* Now Listening...
 app.listen(
   PORT,
   console.log(
