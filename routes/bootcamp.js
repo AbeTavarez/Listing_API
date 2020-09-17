75491ea40e9687da01f322f1ae837db8ded95d71
Abraham E. Tavarez <abetavarez@icloud.com>
2020-09-16 22:18:23 -0400

    changed app to router

--- routes/bootcamp.js
+++ routes/bootcamp.js
@@ -4,23 +4,23 @@ const express = require('express');
 const router = express.Router();
 
 //* Routes
-app.get('/api/v1/bootcamps', (req, res) => {
+router.get('/api/v1/bootcamps', (req, res) => {
   res.status(200).json({ success: true, msg: 'Show All Bootcamps' });
 });
-app.get('/api/v1/bootcamps/:id', (req, res) => {
+router.get('/api/v1/bootcamps/:id', (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `Show Bootcamp ${req.params.id}` });
 });
-app.post('/api/v1/bootcamps', (req, res) => {
+router.post('/api/v1/bootcamps', (req, res) => {
   res.status(200).json({ success: true, msg: 'Create New Bootcamp' });
 });
-app.put('/api/v1/bootcamps/:id', (req, res) => {
+router.put('/api/v1/bootcamps/:id', (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `Update Bootcamp ${req.params.id}` });
 });
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
+router.delete('/api/v1/bootcamps/:id', (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `Delete Bootcamp ${req.params.id}` });
