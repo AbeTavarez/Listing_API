8a8eec242a2559ac70555adb72f4808103e1c539
Abraham E. Tavarez <abetavarez@icloud.com>
2020-09-17 12:43:02 -0400

    installed colors dev dep

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 
 //* Routes Files
 const bootcamps = require('./routes/bootcamps');
@@ -13,7 +14,7 @@ const app = express();
 
 //* Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+  app.use(morgan('dev'.yellow.bold));
 }
 
 //* MOUNT ROUTERS
@@ -29,3 +30,9 @@ app.listen(
     `Server Running in -> ${process.env.NODE_ENV} mode on port ${PORT}...`
   )
 );
+
+//* Handle unhandle promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  consoelo.log(`Error: ${err.message}`.red);
+  //* Close server & Exist process
+});
