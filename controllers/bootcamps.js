062f029876bf035e8a1ea940befacd7a0d3d05dc
Abraham E. Tavarez <abetavarez@icloud.com>
2020-09-17 18:40:18 -0400

    Fetching all bootcamps and single bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,17 +2,31 @@ const Bootcamp = require('../models/Bootcamp');
 //* @desc Get all bootcamps
 //* @route GET /api/v1/bootcamps
 //* @access Public
-exports.getBootCamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show All Bootcamps' });
+exports.getBootCamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //* @desc Get single bootcamps
 //* @route GET /api/v1/bootcamps/:id
 //* @access Public
-exports.getBootCamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show Bootcamp ${req.params.id}` });
+exports.getBootCamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    // makes sure if bootcamp doesnt exist we get 400 status
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //* @desc Create new bootcamp
