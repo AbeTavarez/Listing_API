79497ba5529b45279c6ee1ab226fb59ff7e3effd
Abraham E. Tavarez <abetavarez@icloud.com>
2020-09-18 22:41:29 -0400

    handled mongo duplicate key error

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -11,7 +11,7 @@ exports.getBootCamps = async (req, res, next) => {
       .status(200)
       .json({ success: true, count: bootcamps.length, data: bootcamps });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -32,10 +32,7 @@ exports.getBootCamp = async (req, res, next) => {
 
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    //* If is not a well formatted id
-    next(
-      new ErrorResponce(`Bootcamp NotFound with id of ${req.params.id}`, 404)
-    );
+    next(err);
   }
 };
 
@@ -50,7 +47,7 @@ exports.createBootCamp = async (req, res, next) => {
       data: bootcamp,
     });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -65,11 +62,15 @@ exports.updateBootCamp = async (req, res, next) => {
     });
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      //* Id not in DataBase
+      return new ErrorResponce(
+        `Bootcamp NotFound with id of ${req.params.id}`,
+        404
+      );
     }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -81,10 +82,14 @@ exports.deleteBootCamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      //* Id not in DataBase
+      return new ErrorResponce(
+        `Bootcamp NotFound with id of ${req.params.id}`,
+        404
+      );
     }
     res.status(200).json({ success: true, msg: 'Bootcamp Deleted' });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
--- middleware/error.js
+++ middleware/error.js
@@ -1,11 +1,30 @@
+const ErrorResponse = require('../utils/errorResponce');
+
 //* Error Handler
 const errorHandler = (err, req, res, next) => {
+  //copy from err object
+  let error = { ...err };
+  error.message = err.message;
+
   //* Log to console for developer
-  console.log(err.stack.white.bgRed);
+  console.log(err);
+  // console.log(err.stack.white.bgRed);
+
+  //* Mongoose Bad ObjectID
+  if (err.name == 'CastError') {
+    const message = `Resource Not Found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  //* Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
 
   res
-    .status(err.statusCode || 500)
-    .json({ success: false, error: err.message || 'Server Error' });
+    .status(error.statusCode || 500)
+    .json({ success: false, error: error.message || 'Server Error' });
 };
 
 module.exports = errorHandler;
