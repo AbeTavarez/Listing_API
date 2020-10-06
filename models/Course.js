070365da2bbe47911b3ae2e278eecc676f048db2
Abraham E. Tavarez <abetavarez@icloud.com>
2020-10-05 23:22:49 -0400

    Course model created

---
+++ models/Course.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add a course title'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add a description'],
+  },
+  weeks: {
+    type: String,
+    required: [true, 'Please add a number of weeks'],
+  },
+  tuition: {
+    type: String,
+    required: [true, 'Please add a tuition cost'],
+  },
+  minimumSkill: {
+    type: String,
+    required: [true, 'Please add a minimum skill'],
+    enum: ['beginner', 'intermidiate', 'advanced'],
+  },
+  scholarshipAvailable: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  bootchamp: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Bootcamp',
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
--- seeder.js
+++ seeder.js
@@ -8,7 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 //* Load Models
 const Bootchamp = require('./models/Bootcamp');
-const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 //Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -23,11 +23,16 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
+//* Read JSON files
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
 //* Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
-
+    await Course.create(courses);
     console.log(`Data Imported...`.green.inverse);
     process.exit();
   } catch (err) {
@@ -39,7 +44,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
-
+    await Course.deleteMany();
     console.log(`Data Destroyed...`.red.inverse);
     process.exit();
   } catch (err) {
