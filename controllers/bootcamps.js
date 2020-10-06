120a5c4df94e2efbf145703968350b59629b927a
Abraham E. Tavarez <abetavarez@icloud.com>
2020-10-05 23:09:20 -0400

    next and prev pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -43,17 +43,39 @@ exports.getBootCamps = asyncHandler(async (req, res, next) => {
 
   //* Pagination
   const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 1;
-  const skip = (page - 1) * limit;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
 
-  query = query.skip(skip).limit(limit);
+  query = query.skip(startIndex).limit(limit);
 
   //* Executing query
   const bootcamps = await query;
 
-  res
-    .status(200)
-    .json({ success: true, count: bootcamps.length, data: bootcamps });
+  //* Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    pagination,
+    data: bootcamps,
+  });
 });
 
 //* @desc Get single bootcamps
