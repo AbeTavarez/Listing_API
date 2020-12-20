87d5f74c6a6f4a30b94921124219c33c95674ae1
Abraham E. Tavarez <abetavarez@icloud.com>
2020-12-19 22:49:32 -0500

    password hashing

--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -32,5 +33,11 @@ const UserSchema = new mongoose.Schema({
     }
   },
 });
+
+// Encrypt password using bcrypt
+UserSchema.pre('save', async (next) => {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+})
 
 module.exports = mongoose.model('User', UserSchema);
